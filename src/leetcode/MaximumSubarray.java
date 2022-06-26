d485a8272c0992c23cb3a0e93705597e12d7bf3f
acisliver <7273322@naver.com>
2022-06-26 17:24:31 +0900

    leetcode - MaximumSubarray

--- src/leetcode/MaximumSubarray.java
+++ src/leetcode/MaximumSubarray.java
@@ -1,18 +1,41 @@
 package leetcode;
 
 // https://leetcode.com/problems/maximum-subarray/
-// 동적 프로그래밍
+// 분할 정복
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int[] dp = new int[nums.length];
-        int max = nums[0];
-        dp[0] = nums[0];
+        return dq(nums, 0, nums.length - 1);
+    }
 
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
-            max = Math.max(max, dp[i]);
+    public int dq(int nums[], int start, int end) {
+
+        if (start == end) {
+            return nums[start];
         }
 
-        return max;
+        int mid = (start + end) / 2;
+        int sum = 0;
+        int leftMaxSUM = Integer.MIN_VALUE;
+
+        for (int i = mid; i >= start; i--) {
+            sum += nums[i];
+            if (sum > leftMaxSUM) {
+                leftMaxSUM = sum;
+            }
+        }
+
+        sum = 0;    // reset sum to 0
+        int rightMaxSUM = Integer.MIN_VALUE;
+        for (int i = mid + 1; i <= end; i++) {
+            sum += nums[i];
+            if (sum > rightMaxSUM) {
+                rightMaxSUM = sum;
+            }
+        }
+
+        int maxLeftRight = Math.max(dq(nums, start, mid), dq(nums, mid + 1, end));
+
+        // 왼쪽 혹은 오른쪽의 최대값과 중간에 걸친 최대값 비교
+        return Math.max(maxLeftRight, leftMaxSUM + rightMaxSUM);
     }
 }
