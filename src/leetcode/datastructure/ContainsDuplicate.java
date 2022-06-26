9ab871b81d41bd943bf48d14d5738f6385f55daf
acisliver <7273322@naver.com>
2022-06-26 16:43:31 +0900

    leetcode - ContainsDuplicate

--- src/leetcode/datastructure/ContainsDuplicate.java
+++ src/leetcode/datastructure/ContainsDuplicate.java
@@ -5,14 +5,10 @@ import java.util.*;
 // https://leetcode.com/problems/contains-duplicate/
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums) {
-            int value = map.getOrDefault(num, 0);
-            if (value > 0) return true;
-            else map.put(num, value + 1);
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) return true; // 이미 들어있는 요소의 경우 add()는 false를 리턴한다
         }
-
         return false;
     }
 }
