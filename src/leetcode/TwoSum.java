3e2f89050b1bf8dc6e8e680c1016b7d8fea52545
acisliver <7273322@naver.com>
2022-06-27 22:59:15 +0900

    leetcode - TwoSum

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,19 +1,24 @@
 package leetcode;
 
-// https://leetcode.com/problems/two-sum/
-// 브루트 포스 - 이중포문
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int[] answer = new int[2];
+import java.util.HashMap;
+import java.util.Map;
 
-        int sum;
+// https://leetcode.com/problems/two-sum/
+// HashTable
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                sum = nums[i] + nums[j];
-                if (sum == target) answer = new int[] {i, j};
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
             }
         }
 
-        return answer;
+        return null;
     }
 }
