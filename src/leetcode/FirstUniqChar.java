51741cb4c20312162a28f433aea9712f9349ff84
acisliver <7273322@naver.com>
2022-07-10 17:06:09 +0900

    leetcode - First Unique Character in a String

---
+++ src/leetcode/FirstUniqChar.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/first-unique-character-in-a-string/
+// HashMap
+public class FirstUniqChar {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
