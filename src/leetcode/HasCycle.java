b1d93e5748cb2a46576375de98eef385a95cf4b5
acisliver <7273322@naver.com>
2022-07-11 14:22:17 +0900

    leetcode - Linked List Cycle

--- src/leetcode/HasCycle.java
+++ src/leetcode/HasCycle.java
@@ -1,10 +1,21 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 // https://leetcode.com/problems/linked-list-cycle/
 // LinkedList & Hash
 public class HasCycle {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
     public boolean hasCycle(ListNode list) {
         Map<ListNode, Integer> map = new HashMap<>();
 
