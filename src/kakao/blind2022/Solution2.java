69501fdbc8c5c9d218ffc1b5dac3e12db19a4f7a
Daeeun Ko <7273322@naver.com>
2022-10-14 20:29:09 +0900

    k진소수에서 소수 개수 구하기 - 구현

---
+++ src/kakao/blind2022/Solution2.java
@@ -0,0 +1,26 @@
+package kakao.blind2022;
+
+import java.util.Arrays;
+
+class Solution2 {
+
+    public int solution(int n, int k) {
+
+        return (int) Arrays.stream(Integer.toString(n, k).split("0"))
+                .filter(v -> v.length() != 0)
+                .filter(v -> isPrime(v))
+                .count();
+    }
+
+    private boolean isPrime(String v) {
+        long n = Long.parseLong(v);
+
+        if (n == 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
