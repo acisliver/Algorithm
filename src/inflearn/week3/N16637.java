5e68721ffca2aaf5d82a33fc2073d0bbf3125197
Daeeun Ko <7273322@naver.com>
2023-01-17 06:38:52 +0900

    괄호 추가하기 - DFS, Brute Force

--- src/inflearn/week3/N16637.java
+++ src/inflearn/week3/N16637.java
@@ -3,6 +3,7 @@ package inflearn.week3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 // 괄호 추가하기
@@ -10,21 +11,57 @@ import java.util.List;
 public class N16637 {
 
     static int N, ANSWER;
-    static List<Character> EXPRESSION;
+    static List<Integer> OPERANDS;
+    static List<Character> OPERATORS;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        ANSWER = 0;
-        getMax(0, 1);
+        ANSWER = Integer.MIN_VALUE;
+        String expression = br.readLine();
+        char[] input = expression.toCharArray();
+        OPERANDS = new ArrayList<>();
+        OPERATORS = new ArrayList<>();
+
+        for (int i = 0; i < input.length; i++) {
+            if (i % 2 == 0) OPERANDS.add(Character.getNumericValue(input[i]));
+            else OPERATORS.add(input[i]);
+        }
+
+        getMax(0, OPERANDS.get(0));
+
+        System.out.println(ANSWER);
     }
 
-    private static void getMax(int result, int index) {
-        if (index + 1 == N) {
+    private static void getMax(int index, int result) {
+
+        if (index >= OPERATORS.size()) {
             ANSWER = Math.max(ANSWER, result);
             return;
         }
 
+        // 다음 연산을 먼저하지 않고 진행
+        getMax(index + 1, operate(result, OPERATORS.get(index), OPERANDS.get(index + 1)));
 
+        // 다음 연산을 먼저 하고 진행
+        if (index + 1 < OPERATORS.size()) {
+            int temp = operate(OPERANDS.get(index + 1), OPERATORS.get(index + 1), OPERANDS.get(index + 2));
+            getMax(index + 2, operate(result, OPERATORS.get(index), temp));
+        }
+    }
+
+    private static int operate(int a, char operator, int b) {
+
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+        }
+
+        return 0;
     }
 }
