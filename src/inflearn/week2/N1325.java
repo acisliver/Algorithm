505e2ca939e07e1c98bf2f6ac30e2960409bc09e
Daeeun Ko <7273322@naver.com>
2023-03-06 22:39:56 +0900

    효율적인 해킹 - BFS

--- src/inflearn/week2/N1325.java
+++ src/inflearn/week2/N1325.java
@@ -1,70 +1,64 @@
 package inflearn.week2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 // 효율적인 해킹
 // https://www.acmicpc.net/problem/1325
 public class N1325 {
 
-    static int N, M, MAX_HACKED_COMPUTER;
-    static List<Integer> HACKED_START_COMPUTERS;
-    static List<List<Integer>> COMPUTERS;
+    static int N, M;
+    static List<List<Integer>> GRAPH = new ArrayList<>();
+    static int[] COUNTS;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        N = input[0];
-        M = input[1];
-        MAX_HACKED_COMPUTER = 0;
-        HACKED_START_COMPUTERS = new LinkedList<>();
-        COMPUTERS = new ArrayList<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < N; i++) {
-            COMPUTERS.add(new LinkedList<>());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= N; i++) {
+            GRAPH.add(new ArrayList<>());
         }
-
         for (int i = 0; i < M; i++) {
-            input = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            int slave = input[0] - 1;
-            int master = input[1] - 1;
-            COMPUTERS.get(master).add(slave);
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            GRAPH.get(b).add(a);
         }
 
-        for (int computer = 0; computer < N; computer++) {
-            int hacked = hack(computer, new boolean[N]);
-            if (hacked > MAX_HACKED_COMPUTER) {
-                HACKED_START_COMPUTERS.clear();
-                MAX_HACKED_COMPUTER = hacked;
-                HACKED_START_COMPUTERS.add(computer + 1);
-            } else if (hacked == MAX_HACKED_COMPUTER) {
-                HACKED_START_COMPUTERS.add(computer + 1);
+        COUNTS = new int[N + 1];
+        int max = 0;
+        for (int i = 1; i <= N; i++) {
+            bfs(i);
+            max = Math.max(COUNTS[i], max);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (COUNTS[i] == max) bw.write(i + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void bfs(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+        queue.add(n);
+        visited[n] = true;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (Integer next : GRAPH.get(cur)) {
+                if (visited[next]) continue;
+                queue.add(next);
+                visited[next] = true;
+                COUNTS[n] += 1;
             }
         }
-
-        Collections.sort(HACKED_START_COMPUTERS);
-
-        System.out.println(HACKED_START_COMPUTERS.stream().map(String::valueOf).collect(Collectors.joining(" ")));
     }
-
-    private static int hack(int master,  boolean[] visited) {
-        visited[master] = true;
-        int hacked = 1;
-
-        for (Integer slave : COMPUTERS.get(master)) {
-            if (visited[slave]) continue;
-            hacked += hack(slave, visited);
-        }
-
-        return hacked;
-    }
-
-
 }
