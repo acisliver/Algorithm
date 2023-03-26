9b423fd186af20e0a9d73bbf6a8d63c7688e1497
Daeeun Ko <7273322@naver.com>
2023-03-26 17:31:26 +0900

    경찰차 - DP

--- src/inflearn/week8/N2618.java
+++ src/inflearn/week8/N2618.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Stack;
 
 // 경찰차
 // https://www.acmicpc.net/problem/2618
@@ -20,63 +19,45 @@ public class N2618 {
         N = Integer.parseInt(br.readLine());
         W = Integer.parseInt(br.readLine());
         MAP = new boolean[N + 1][N + 1];
-        CASES = new int[W][2];
-        DP = new int[W][2];
-        for (int i = 0; i < W; i++) {
+        CASES = new int[W + 2][2];
+        DP = new int[1004][1004];
+        CASES[0] = new int[]{1, 1};
+        CASES[1] = new int[]{N, N};
+        for (int i = 2; i < W + 2; i++) {
             CASES[i] = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
         }
 
-        DP[0][0] = getDistance(new int[]{1, 1}, CASES[0]);
-        DP[0][1] = getDistance(new int[]{N, N}, CASES[0]);
-        DP[1][0] = Math.min(
-                getDistance(CASES[0], CASES[1]) + DP[0][0],
-                getDistance(new int[]{1, 1}, CASES[1]) + DP[0][1]
-        );
-        DP[1][1] = Math.min(
-                getDistance(CASES[0], CASES[1]) + DP[0][1],
-                getDistance(new int[]{N, N}, CASES[1]) + DP[0][0]
-        );
-        for (int i = 2; i < W; i++) {
-            DP[i][0] = Math.min(
-                getDistance(CASES[i - 1], CASES[i]) + DP[i - 1][0],
-                getDistance(CASES[i - 2], CASES[i]) + DP[i - 1][1]
-            );
-            DP[i][1] = Math.min(
-                    getDistance(CASES[i - 1], CASES[i]) + DP[i - 1][1],
-                    getDistance(CASES[i - 2], CASES[i]) + DP[i - 1][0]
-            );
-        }
-
-        System.out.println(Math.min(DP[W - 1][0], DP[W - 1][1]));
-        printChoice();
+        System.out.println(go(0, 1));
+        trace();
     }
 
-    private static void printChoice() {
-        Stack<Integer> stack = new Stack<>();
-        int curCar;
-        if (DP[W - 1][0] > DP[W - 1][1]) {
-            curCar = 2;
-        } else {
-            curCar = 1;
-        }
-        stack.push(curCar);
-
-        for (int i = W - 2; i >= 0; i--) {
-            int car1 = curCar - DP[i][0];
-            int car2 = curCar - DP[i][1];
-            int dist = getDistance(CASES[i + 1], CASES[i]);
-            if (car1 == dist) stack.push(1);
-            else stack.push(2);
-        }
-
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
-        }
+    private static int go(int a, int b) {
+        if (a == W + 1 || b == W + 1) return 0;
+        if (DP[a][b] != 0) return DP[a][b];
+        int next = Math.max(a, b) + 1;
+        DP[a][b] = Math.min(go(a, next) + getDistance(b, next), go(next, b) + getDistance(a, next));
+        return DP[a][b];
     }
 
-    private static int getDistance(int[] p1, int[] p2) {
+    private static int getDistance(int a, int b) {
+        int[] p1 = CASES[a];
+        int[] p2 = CASES[b];
         return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+    }
+
+    private static void trace() {
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i < W + 2; i++) {
+            if (DP[i][b] + getDistance(a, i) < DP[a][i] + getDistance(b, i)) {
+                a = i;
+                System.out.println(1);
+            } else {
+                b = i;
+                System.out.println(2);
+            }
+        }
     }
 }
