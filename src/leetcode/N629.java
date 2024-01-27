dd4ab990c79c06f7abec3ebf3aa1e29db8616625
Daeeun Ko <7273322@naver.com>
2024-01-27 23:58:51 +0900

    K Inverse Pairs Array - DP

---
+++ src/leetcode/N629.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class N629 {
+    private static final int MOD = 1_000_000_007;
+
+    public int kInversePairs(int n, int k) {
+        int[][] dp = new int[n + 1][k + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                for (int x = 0; x <= Math.min(j, i - 1); x++) {
+                    if (j - x >= 0) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % MOD;
+                    }
+                }
+            }
+        }
+        return dp[n][k];
+    }
+}
