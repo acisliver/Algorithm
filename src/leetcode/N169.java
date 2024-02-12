f13644dc96c5ab1bd4de1891b0b6237751552141
Daeeun Ko <7273322@naver.com>
2024-02-12 09:32:17 +0900

    Majority Element - Moore Voting

---
+++ src/leetcode/N169.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+// Majority Element
+public class N169 {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+
+        return candidate;
+    }
+}
