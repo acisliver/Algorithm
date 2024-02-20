881f7fc72a91cf363c48abe5ac3c4e8be5b4a23f
Daeeun Ko <7273322@naver.com>
2024-02-20 14:39:53 +0900

    Missing Number - 수학

---
+++ src/leetcode/N268.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+// Missing Number
+public class N268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+        for (int num : nums) {
+            sum -= num;
+        }
+        return sum;
+    }
+}
