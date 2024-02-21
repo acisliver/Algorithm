eb73232b64322d8663de960882fac0e9a48c72a8
Daeeun Ko <7273322@naver.com>
2024-02-21 21:43:57 +0900

    Bitwise AND of Numbers Range - 비트연산

--- src/leetcode/N201.java
+++ src/leetcode/N201.java
@@ -8,7 +8,13 @@ public class N201 {
     }
 
     public int rangeBitwiseAnd(int left, int right) {
-        int shift = 31 ^ Integer.numberOfLeadingZeros(left ^ right | 1);
-        return left >>> shift << shift;
+        int count = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            count += 1;
+        }
+
+        return (left << count);
     }
 }
