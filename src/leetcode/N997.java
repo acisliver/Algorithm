fb7ad70d887ad01e4c188ed086a85a7f0cb24131
Daeeun Ko <7273322@naver.com>
2024-02-22 11:07:04 +0900

    Find the Town Judge - Array

--- src/leetcode/N997.java
+++ src/leetcode/N997.java
@@ -5,41 +5,21 @@ import java.util.*;
 // Find the Town Judge
 public class N997 {
     public int findJudge(int n, int[][] trust) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            map.put(i, new HashSet<>());
+        int[] count = new int[n];
+
+        for (int[] t : trust) {
+            int a = t[0] - 1;
+            int b = t[1] - 1;
+            count[a] -= 1;
+            count[b] += 1;
         }
 
-        for (int[] tr : trust) {
-            int a = tr[0];
-            int b = tr[1];
-            map.get(a).add(b);
-        }
-
-        System.out.println(map);
-
-        // 1. town judge trusts nobody
-        int candidate = -1;
-        for (var entry : map.entrySet()) {
-            if (entry.getValue().size() == 0) {
-                if (candidate == -1) {
-                    candidate = entry.getKey();
-                } else {
-                    return -1;
-                }
+        for (int i = 0; i < n; i++) {
+            if (count[i] == n - 1) {
+                return i + 1;
             }
         }
 
-        // 2. EveryBody trusts the town judge (except for the town judge)
-        for (var entry : map.entrySet()) {
-            if (entry.getKey() == candidate) {
-                continue;
-            }
-            if (!entry.getValue().contains(candidate)) {
-                return -1;
-            }
-        }
-
-        return candidate;
+        return -1;
     }
 }
