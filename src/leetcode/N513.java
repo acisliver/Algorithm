716aad8371701f0646ef42667fda0b7a7872f992
Daeeun Ko <7273322@naver.com>
2024-02-28 23:25:26 +0900

    Create N513.java
    Find Bottom Left Tree Value - BFS

---
+++ src/leetcode/N513.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int answer = 0;
+        while (!q.isEmpty()) {
+            answer = q.peek().val;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = q.poll();
+                if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+  static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+  }
+ 
+}
