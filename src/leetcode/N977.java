e06f68877a50927d6b201ce433433f641b220d89
Daeeun Ko <7273322@naver.com>
2024-03-02 17:14:02 +0900

    Squares of a Sorted Array - Two Pointer

--- src/leetcode/N977.java
+++ src/leetcode/N977.java
@@ -5,9 +5,24 @@ import java.util.Arrays;
 // Squares of a Sorted Array
 public class N977 {
     public int[] sortedSquares(int[] nums) {
-        return Arrays.stream(nums)
-                .map(n -> n * n)
-                .sorted()
-                .toArray();
+        int i = nums.length - 1;
+        int l = 0;
+        int r = nums.length - 1;
+        int[] answer = new int[nums.length];
+
+        while (l <= r) {
+            int lVal = nums[l] * nums[l];
+            int rVal = nums[r] * nums[r];
+
+            if (lVal < rVal) {
+                answer[i--] = rVal;
+                r -= 1;
+            } else {
+                answer[i--] = lVal;
+                l += 1;
+            }
+        }
+
+        return answer;
     }
 }
