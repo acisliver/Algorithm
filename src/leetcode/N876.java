8f2956f40cf8f66de545df00b570240184a97e0e
Daeeun Ko <7273322@naver.com>
2024-03-07 09:22:03 +0900

    Middle of the Linked List - Two Pointer

---
+++ src/leetcode/N876.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+// Middle of the Linked List
+public class N876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
