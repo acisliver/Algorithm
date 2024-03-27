5f644c902739c63024d58e5ec176feed9c66bd02
Daeeun Ko <7273322@naver.com>
2024-03-27 11:45:16 +0900

    Subarray Product Less Than K - Sliding Window
    O(n^2)도 알고리즘에 따라 실행 시간이 다르다.

--- src/leetcode/N713.java
+++ src/leetcode/N713.java
@@ -1,12 +1,8 @@
 package leetcode;
 
-// https://leetcode.com/problems/subarray-product-less-than-k/
-// Sliding Window
+// Subarray Product Less Than K
 public class N713 {
 
-    // 풀이
-    // k값을 윈도우로 두고 k값보다 큰 값이 나타나면
-    // 값을 줄이려고 왼쪽 포인터를 감소시킨다.
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if (k == 0) return 0;
 
@@ -16,13 +12,13 @@ public class N713 {
         int product = 1;
 
         while (right < nums.length) {
-            product *= nums[right]; // 우선 product를 곱하며 subarray의 범위를 늘린다.
+            product *= nums[right];
 
-            while (left <= right && product >= k) {  // k보다 큰 값 나타남 -> 윈도우를 넘어섬
+            while (left <= right && product >= k) {
                 product /= nums[left++];
             }
 
-            answer += right - left + 1; // 이 부분에 대해선 블로그에 정리하겠다.
+            answer += right - left + 1;
             right++;
         }
 
