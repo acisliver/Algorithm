34f13cbd026536c03cc451207c471c7cc234ff86
Daeeun Ko <7273322@naver.com>
2024-04-04 15:11:03 +0900

    Maximum Nesting Depth of the Parentheses - String

---
+++ src/kakao/intern2024/Solution3.java
@@ -0,0 +1,12 @@
+package kakao.intern2024;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/258709
+public class Solution3 {
+    public int[] solution(int[][] dice) {
+        int[] answer = {};
+
+        
+
+        return answer;
+    }
+}
