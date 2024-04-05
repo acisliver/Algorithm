47114548f95ba4abc25297ab680d6b3ccd597a62
Daeeun Ko <7273322@naver.com>
2024-04-05 19:12:24 +0900

    Make The String Great - Stack

--- src/leetcode/N1544.java
+++ src/leetcode/N1544.java
@@ -1,3 +1,26 @@
 package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// Make The String Great
 public class N1544 {
+    public String makeGood(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+
+        return sb.toString();
+    }
 }
