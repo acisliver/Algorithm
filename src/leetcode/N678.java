3eec5579f16a855effed1c9033c1de292a65799b
Daeeun Ko <7273322@naver.com>
2024-04-08 00:10:39 +0900

    Valid Parenthesis String - DP

--- src/leetcode/N678.java
+++ src/leetcode/N678.java
@@ -1,31 +1,43 @@
 package leetcode;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Arrays;
 
 // Valid Parenthesis String
 public class N678 {
     public boolean checkValidString(String s) {
-        List<Character> list = new LinkedList<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '*') {
-                list.add(c);
-            } else {
-                int idxO = list.lastIndexOf('(');
-                int idxA = list.lastIndexOf('*');
-                if (idxO < 0 && idxA < 0) {
-                    return false;
-                }
-                if (idxO < 0) {
-                    list.remove(idxA);
-                }
-                if (idxA < 0) {
-                    list.remove(idxO);
-                }
+        int[][] memo = new int[s.length()][s.length()];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+        return isValidString(0, 0, s, memo);
+    }
+
+    private boolean isValidString(int i, int openCount, String s, int[][] memo) {
+        if (i == s.length()) {
+            return openCount == 0;
+        }
+
+        if (memo[i][openCount] != -1) {
+            return memo[i][openCount] == 1;
+        }
+
+        boolean isValid = false;
+
+        if (s.charAt(i) == '*') {
+            isValid |= isValidString(i + 1, openCount + 1, s, memo);
+            if (openCount > 0) {
+                isValid |= isValidString(i + 1, openCount - 1, s, memo);
+            }
+            isValid |= isValidString(i + 1, openCount, s, memo);
+        } else {
+            if (s.charAt(i) == '(') {
+                isValid |= isValidString(i + 1, openCount + 1, s, memo);
+            } else if (openCount > 0) {
+                isValid |= isValidString(i + 1, openCount - 1, s, memo);
             }
         }
 
-
-        return list.isEmpty();
+        memo[i][openCount] = isValid ? 1 : 0;
+        return isValid;
     }
 }
